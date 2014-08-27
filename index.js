5a950643283794c78bf96c61838c0cd819951b8d
jasonfutch <pth58vfx>
2014-08-27 13:55:21 -0400

    .

--- index.js
+++ index.js
@@ -26,4 +26,4 @@ exports.update      = $update;
 exports.validators  = $validators;
 
 exports.data        = {};
-exports.loadData     = function(dir){ exports.data = $dataTables.load(dir) };
+exports.loadData    = function(dir){ exports.data = $dataTables.load(dir) };
