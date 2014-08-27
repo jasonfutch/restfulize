1e4214e44745fd9733d0c69940ae595cfad4d346
jasonfutch <pth58vfx>
2014-08-27 13:52:36 -0400

    .

--- index.js
+++ index.js
@@ -6,20 +6,24 @@ var $get            = require('./lib/get');
 var $insert         = require('./lib/insert');
 var $list           = require('./lib/list');
 var $map            = require('./lib/map');
+var $sql            = require('./lib/mysql');
 var $request        = require('./lib/request');
 var $sqlString      = require('./lib/sql-string');
 var $update         = require('./lib/update');
 var $validators     = require('./lib/validators');
 
 exports.actions     = $actions;
-exports.data        = $dataTables;
 exports.errors      = $errors;
 exports.filters     = $filters;
 exports.get         = $get;
 exports.insert      = $insert;
 exports.list        = $list;
 exports.map         = $map;
+exports.sql         = $sql;
 exports.request     = $request;
 exports.sqlString   = $sqlString;
 exports.update      = $update;
 exports.validators  = $validators;
+
+exports.data        = {};
+exports.loadData     = function(dir){ exports.data = $dataTables.load(dir) };
